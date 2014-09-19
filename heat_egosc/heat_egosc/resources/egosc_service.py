e3290714154244c08d3941668edf9b59371742aa
gyliu <gyliu@devstack007.cn.ibm.com>
2014-09-19 16:11:12 +0800

    add ego service

--- heat_egosc/heat_egosc/resources/egosc_service.py
+++ heat_egosc/heat_egosc/resources/egosc_service.py
@@ -26,7 +26,6 @@ from heat.openstack.common import log as logging
 LOG = logging.getLogger(__name__)
 
 EGO_INSTALLED = False
-EGO_INSTALLED = True
 ego = None
 # conditionally import so tests can work without having the dependency
 # satisfied
@@ -47,33 +46,48 @@ except ImportError:
 class EGOService(resource.Resource):
 
     PROPERTIES = (
-        NAME, DESCRIPTION, MININSTANCES, MAXINSTANCES,
+        DESCRIPTION, MIN_INSTANCES, MAX_INSTANCES, CONTROL_POLICY,
     ) = (
-        'name', 'description', 'mininstances', 'maxinstances',
+        'description', 'min_instances', 'max_instances', 'control_policy',
     )
 
+    _CONTROL_POLICY_KEYS = (
+        START_TYPE, MAX_RESTARTS,
+    ) = (
+        'start_type', 'max_restarts',
+    )
     properties_schema = {
-        NAME: properties.Schema(
-            properties.Schema.STRING,
-            _('EGO Service Name'),
-            required=True
-        ),
         DESCRIPTION: properties.Schema(
             properties.Schema.STRING,
-            _('Docker daemon endpoint (by default the local docker daemon '
-              'will be used).'),
+            _('EGO Service Description.'),
             default='EGO Service'
         ),
-        MININSTANCES: properties.Schema(
+        MIN_INSTANCES: properties.Schema(
             properties.Schema.INTEGER,
-            _('Hostname of the container.'),
+            _('Min Number of Instances.'),
             default=1
         ),
-        MAXINSTANCES: properties.Schema(
+        MAX_INSTANCES: properties.Schema(
             properties.Schema.INTEGER,
-            _('Username or UID.'),
+            _('Max Number of Instances.'),
             default=1
         ),
+        CONTROL_POLICY: properties.Schema(
+            properties.Schema.MAP,
+            _('EGO Service Controll Policy.'),
+            default={'start_type': 'manual', 'max_restarts': 10},
+            schema={
+                START_TYPE: properties.Schema(
+                    properties.Schema.STRING,
+                    _('Service Start Type'),
+                ),
+                MAX_RESTARTS: properties.Schema(
+                    properties.Schema.INTEGER,
+                    _('Service Max Restart Time.'),
+                ),
+            }
+        ),
+        
     }
 
     def get_client(self):
@@ -82,31 +96,41 @@ class EGOService(resource.Resource):
             client = ego.ego()
         return client
 
-    def handle_create(self):
-        client = self.get_client()
-        xml_str = '<?xml version="1.0" encoding="UTF-8"?> \
+    def _get_service_header(self):
+        return '<?xml version="1.0" encoding="UTF-8"?> \
 <sc:ServiceDefinition xmlns:sc="http://www.platform.com/ego/2005/05/schema/sc" xmlns:ego="http://www.platform.com/ego/2005/05/schema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xsi:schemaLocation="http://www.platform.com/ego/2005/05/schema/sc ../sc.xsd http://www.platform.com/ego/2005/05/schema ../ego.xsd" ServiceName="%s"> \
-  <sc:Version>1.2</sc:Version> \
-  <sc:Description>%s</sc:Description> \
+        <sc:Version>1.2</sc:Version> ' % (self.name) 
+
+    def _get_service_instance_conf(self):
+        return '<sc:Description>%s</sc:Description> \
   <sc:MinInstances>%d</sc:MinInstances> \
   <sc:MaxInstances>%d</sc:MaxInstances> \
   <sc:Priority>100</sc:Priority> \
   <sc:MaxInstancesPerSlot>1</sc:MaxInstancesPerSlot> \
-  <sc:NeedCredential>TRUE</sc:NeedCredential> \
-  <sc:ControlPolicy> \
-    <sc:StartType>MANUAL</sc:StartType> \
-    <sc:MaxRestarts>10</sc:MaxRestarts> \
+  <sc:NeedCredential>TRUE</sc:NeedCredential> ' % (self.properties[self.DESCRIPTION],
+                                                   self.properties[self.MIN_INSTANCES],
+                                                   self.properties[self.MAX_INSTANCES],)
+
+    def _get_controll_policy(self):
+        return '<sc:ControlPolicy> \
+    <sc:StartType>%s</sc:StartType> \
+    <sc:MaxRestarts>%d</sc:MaxRestarts> \
     <sc:HostFailoverInterval>PT1M0S</sc:HostFailoverInterval> \
-  </sc:ControlPolicy> \
-  <sc:AllocationSpecification> \
+  </sc:ControlPolicy> ' %(self.properties[self.CONTROL_POLICY][self.START_TYPE],
+                          self.properties[self.CONTROL_POLICY][self.MAX_RESTARTS])
+
+    def _get_allocation_spec(self):
+        return '<sc:AllocationSpecification> \
     <ego:ConsumerID>/ManagementServices/EGOManagementServices</ego:ConsumerID> \
     <!--The ResourceType specifies a "compute element" identified by the URI used below--> \
     <sc:ResourceSpecification ResourceType="http://www.platform.com/ego/2005/05/schema/ce"> \
       <ego:ResourceGroupName>ManagementHosts</ego:ResourceGroupName> \
       <ego:ResourceRequirement>select(!NTIA64 &amp;&amp; !SOL64)</ego:ResourceRequirement> \
     </sc:ResourceSpecification> \
-  </sc:AllocationSpecification> \
-  <sc:ActivityDescription> \
+  </sc:AllocationSpecification> '
+
+    def _get_activity_desc(self):
+        return '<sc:ActivityDescription> \
     <ego:Attribute name="hostType" type="xsd:string">all</ego:Attribute> \
     <ego:ActivitySpecification> \
       <ego:Command>sleep 1000</ego:Command> \
@@ -114,13 +138,17 @@ class EGOService(resource.Resource):
       <ego:Umask>0777</ego:Umask> \
     </ego:ActivitySpecification> \
   </sc:ActivityDescription> \
-</sc:ServiceDefinition>' %(self.properties[self.NAME],
-                           self.properties[self.DESCRIPTION],
-                           self.properties[self.MININSTANCES],
-                           self.properties[self.MAXINSTANCES])
+</sc:ServiceDefinition>'
+
+    def handle_create(self):
+        client = self.get_client()
+        xml_str = (self._get_service_header() + self._get_service_instance_conf()
+                  + self._get_controll_policy() + self._get_allocation_spec()
+                  + self._get_activity_desc())
+
         result = client.esc_create_service(xml_str)
-        self.resource_id_set(self.properties[self.NAME])
-        return self.NAME
+        self.resource_id_set(self.name)
+        return self.resource_id
 
     def check_create_complete(self, service_name):
         return True
@@ -162,5 +190,5 @@ def available_resource_mapping():
     if EGO_INSTALLED:
         return resource_mapping()
     else:
-        LOG.warn(_("Docker plug-in loaded, but docker lib not installed."))
+        LOG.warn(_("EGO was not installed."))
         return {}
