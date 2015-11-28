837826688a5856f83db1c74b2670f98fb42119ac
Guang Ya Liu <liugya@cn.ibm.com>
2015-11-28 16:45:59 +0800

    Comparing Kubernetes

--- dcos/kubernetes/build.sh
+++ dcos/kubernetes/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-GO_URL=https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
+#GO_URL=https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
+GO_URL=https://storage.googleapis.com/golang/go1.4.linux-amd64.tar.gz
 
 TOP_DIR=`pwd`
 
@@ -8,7 +9,8 @@ sudo apt-get install -y wget
 
 #Install Go 
 if  [ ! -d $TOP_DIR/go ] ; then
-  wget $GO_URL
+  curl -O -L $GO_URL
+  #wget $GO_URL
   tar zxvf $(basename $GO_URL)
 fi
 
@@ -20,6 +22,7 @@ export PATH=$GOROOT/bin:$PATH
 mkdir -p $GOPATH
 
 git clone https://github.com/GoogleCloudPlatform/kubernetes
+git checkout -b release-1.1 -t remotes/origin/release-1.1
 
 cd kubernetes
 
@@ -29,12 +32,19 @@ make
 
 cd $TOP_DIR
 
-echo "FROM $HUB_USER/kubernetes" > Dockerfile.km
+#echo "FROM $HUB_USER/kubernetes" > Dockerfile.km
+#cat Dockerfile.km.template >> Dockerfile.km
+
+#echo "FROM $HUB_USER/kubernetes" > Dockerfile.kubelet
+#cat Dockerfile.kubelet.template >> Dockerfile.kubelet
+
+cat Dockerfile.kubernetes > Dockerfile.km
 cat Dockerfile.km.template >> Dockerfile.km
 
-echo "FROM $HUB_USER/kubernetes" > Dockerfile.kubelet
+cat Dockerfile.kubernetes > Dockerfile.kubelet
 cat Dockerfile.kubelet.template >> Dockerfile.kubelet
 
-sudo docker build -t $HUB_USER/kubernetes -f Dockerfile.kubernetes --no-cache .
+
+#sudo docker build -t $HUB_USER/kubernetes -f Dockerfile.kubernetes --no-cache .
 sudo docker build -t $HUB_USER/km -f Dockerfile.km --no-cache .
 sudo docker build -t $HUB_USER/kubelet -f Dockerfile.kubelet --no-cache .
