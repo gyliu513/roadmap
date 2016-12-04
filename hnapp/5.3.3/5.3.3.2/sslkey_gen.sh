c8a2eac8e19d166f366c69e70221e0c21fa30dd6
Hui Chen <hchenxa@cn.ibm.com>
2016-12-04 19:14:44 +0800

    update tls

--- hnapp/5.3.3/5.3.3.2/sslkey_gen.sh
+++ hnapp/5.3.3/5.3.3.2/sslkey_gen.sh
@@ -1,1 +1,11 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /tmp/tls.key -out /tmp/tls.crt -subj "/CN=tomcat.com"
+
+echo "
+apiVersion: v1
+kind: Secret
+metadata:
+  name: tls
+data:
+  tls.crt: `base64 -w 0 /tmp/tls.crt`
+  tls.key: `base64 -w 0 /tmp/tls.key`
+" > ssl.yaml
