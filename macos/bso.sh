1db52e2fa241689ea5b629258dd9da995c7c9023
Guang Ya Liu <gyliu513@gmail.com>
2018-12-04 17:42:21 +0800

    Create bso.sh

---
+++ macos/bso.sh
@@ -0,0 +1,34 @@
+```shell
+#!/usr/bin/expect
+set timeout 5
+set userName liugya@cn.ibm.com
+set password xxx
+
+spawn telnet 9.111.250.181
+expect "Username:"
+send "$userName\r"
+expect "Password:"
+send "$password\r";
+expect eof
+
+spawn telnet 9.21.63.60
+expect "Username:"
+send "$userName\r"
+expect "Password:"
+send "$password\r";
+expect eof
+
+spawn telnet 9.111.143.202
+expect "Username:"
+send "$userName\r"
+expect "Password:"
+send "$password\r";
+expect eof
+
+spawn telnet x1
+expect "Username:"
+send "$userName\r"
+expect "Password:"
+send "$password\r";
+expect eof
+```
